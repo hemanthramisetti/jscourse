8db7468e8534b932ba0965ebec4448ae2c48aecb
hemanthramisetti <ramisettihemanth2@gmail.com>
2025-08-13 20:46:48 +0530

    Add files via upload

---
+++ DAY6/script.js
@@ -0,0 +1,20 @@
+document.getElementById("section1-link")
+.addEventListener("click", function() {
+    document.getElementById("section1").scrollIntoView({behavior: "smooth"})
+})
+document.getElementById("section2-link")
+.addEventListener("click", function() {
+    document.getElementById("section2").scrollIntoView({behavior: "smooth"})
+})
+document.getElementById("section3-link")
+.addEventListener("click", function() {
+    document.getElementById("section3").scrollIntoView({behavior: "smooth"})
+})
+document.getElementById("section4-link")
+.addEventListener("click", function() {
+    document.getElementById("section4").scrollIntoView({behavior: "smooth"})
+})
+document.getElementById("section5-link")
+.addEventListener("click", function() {
+    document.getElementById("section5").scrollIntoView({behavior: "smooth"})
+})
